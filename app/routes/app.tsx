549d042d6187198518e84b88bd16266a8b90989f
Lorenzo <lorenzoccv@gmail.com>
2026-06-03 02:05:37 -0300

    disabled billing temp

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -6,20 +6,7 @@ import { AppProvider } from "@shopify/shopify-app-react-router/react";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const { billing } = await authenticate.admin(request);
-
-  const { hasActivePayment } = await billing.check({
-    plans: ["ComboLoco Pro"],
-    isTest: process.env.NODE_ENV !== "production",
-  });
-
-  if (!hasActivePayment) {
-    await billing.request({
-      plan: "ComboLoco Pro",
-      isTest: process.env.NODE_ENV !== "production",
-      returnUrl: process.env.SHOPIFY_APP_URL,
-    });
-  }
+  await authenticate.admin(request);
 
   // eslint-disable-next-line no-undef
   return { apiKey: process.env.SHOPIFY_API_KEY || "" };
